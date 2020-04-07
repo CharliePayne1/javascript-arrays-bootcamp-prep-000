65a9623d4f1544cb77b8e08e8c04ef4582cde3c7
Charlie Payne <charliepayne1101@gmail.com>
2020-04-07 16:18:59 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,34 +1,11 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-var candyString = ["foo"];
+var candyString = ['foo'];
 
-function addElementToBeginningOfArray(chocolateBars, candyString) {
-  return [candyString, ...chocolateBars];
-}
 function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString) {
   chocolateBars.unshift(candyString);
-  return chocolateBars;
+  return chocolateBars
 }
-function addElementToEndOfArray(chocolateBars, candyString) {
-  return [...chocolateBars, candyString];
-}
-function destructivelyAddElementToEndOfArray(chocolateBars, candyString) {
-  chocolateBars.push(candyString);
-  return chocolateBars;
-}
-function accessElementInArray(chocolateBars, n) {
-  return chocolateBars[n];
-}
-function destructivelyRemoveElementFromBeginningOfArray(array) {
-  array.shift();
-  return array;
-}
-function destructivelyRemoveElementFromEndOfArray(array) {
-  array.pop();
-  return array;
-}
-function removeElementFromBeginningOfArray(array) {
-  return array.slice(1);
-}
-function removeElementFromEndOfArray(array) {
-  return array.slice(0, array.length -1);
+
+function addElementToBeginningOfArray(chocolateBars, candyString){
+  return [candyString, ...chocolateBars];
 }
